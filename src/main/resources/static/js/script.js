2de25fe0fa2d7c7c7e7e735cce145e54a4807836
Bhavishya Jasuja <bhavishya.jasuja@healthcatalyst.com>
2024-07-20 21:35:27 +0530

    implemented the functionality of dark and light theme button

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,21 +1,48 @@
-let currentTheme= getTheme();
+let currentTheme = getTheme();
 
+//initial-->
+document.addEventListener("DOMContentLoaded", () => {
+  changeTheme();
+});
 //todo
 
-function changeTheme()
-{
+function changeTheme() {
+  //set the web page
+  document.querySelector("html").classList.add(currentTheme);
 
+  //set the listner to change theme button
+  const changeThemebutton = document.getElementById("theme_change_button");
+
+  changeThemebutton.addEventListener("click", (event) => {
+    const oldtheme = currentTheme;
+
+    if (currentTheme === "dark") {
+      currentTheme = "light";
+    } else {
+      currentTheme = "dark";
+    }
+
+    //update the localstorage now
+    setTheme(currentTheme);
+
+    //remove the old theme
+    document.querySelector("html").classList.remove(oldtheme);
+    //set the current theme
+    document.querySelector("html").classList.add(currentTheme);
+
+    // change the text of currenttheme
+    changeThemebutton.querySelector("span").textContent =
+      currentTheme == "light" ? "Dark" : "Light";
+  });
 }
 
 //set theme to localStorage
-function setTheme(theme)
-{
-    localStorage.setItem("theme", theme);
+function setTheme(theme) {
+  localStorage.setItem("theme", theme);
 }
 
 //get item from localStorage
-function getTheme()
-{
-    let theme=localStorage.getItem("theme");
-    return theme ? theme : "light";
+function getTheme() {
+  let theme = localStorage.getItem("theme");
+  return theme ? theme : "light";
 }
